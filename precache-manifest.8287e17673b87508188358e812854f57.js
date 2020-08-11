255a1deca4c1ff107103f3a4385cd9a2623c1aeb
joshinasla <joshinasla18@gmail.com>
2020-08-11 13:54:48 +0545

    Updates

---
+++ precache-manifest.8287e17673b87508188358e812854f57.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95008ed3231088c308c592ec5bc3560",
+    "url": "/react-assignment-1/index.html"
+  },
+  {
+    "revision": "2c19f42b93337481c0f2",
+    "url": "/react-assignment-1/static/css/main.1632003d.chunk.css"
+  },
+  {
+    "revision": "10ecfa186d16d3a9dd2d",
+    "url": "/react-assignment-1/static/js/2.7e1ad799.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-assignment-1/static/js/2.7e1ad799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c19f42b93337481c0f2",
+    "url": "/react-assignment-1/static/js/main.a00d13ce.chunk.js"
+  },
+  {
+    "revision": "dc982be8db1249af2a88",
+    "url": "/react-assignment-1/static/js/runtime-main.cd252c77.js"
+  },
+  {
+    "revision": "d049032c1755b119208c038fc2978d7d",
+    "url": "/react-assignment-1/static/media/basketball.d049032c.png"
+  },
+  {
+    "revision": "455ad6bcd4ce611535e8095ec2d2bd97",
+    "url": "/react-assignment-1/static/media/cooking.455ad6bc.png"
+  },
+  {
+    "revision": "bcc8eadb4561b263beeaa7cb8534da28",
+    "url": "/react-assignment-1/static/media/drawing.bcc8eadb.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-assignment-1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "90d1a79ff365094837ee7833cb98c887",
+    "url": "/react-assignment-1/static/media/makeup.90d1a79f.png"
+  },
+  {
+    "revision": "407d8a4eea15a3f397cf1d6ce453adb2",
+    "url": "/react-assignment-1/static/media/photo.407d8a4e.JPG"
+  }
+]);
